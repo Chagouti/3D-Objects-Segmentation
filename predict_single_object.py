8de9adbb7971757d511441fa0236c72090d0b40b
EL HOUCINE CHAGOUTI <138931526+Chagouti@users.noreply.github.com>
2024-10-10 13:08:31 +0100

    Update predict_single_object.py

--- predict_single_object.py
+++ predict_single_object.py
@@ -83,12 +83,8 @@ def main(args):
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     
-
-    # Change dataset path to point to your IntrA dataset
-    data_path = '/content/'  # Path to IntrA dataset
-    
     # Replace ModelNetDataLoader with your dataset loader
-    test_dataset = IntrADataLoader(root=data_path, num_point=args.num_point, num_category=args.num_category, use_uniform_sample=args.use_uniform_sample, use_normals=args.use_normals,state='test', split='test', process_data=False)
+    test_dataset = IntrADataLoader(ROOT_DIR, num_point=args.num_point, num_category=args.num_category, use_uniform_sample=args.use_uniform_sample, use_normals=args.use_normals,state='test', split='test', process_data=False)
     num_workers = 0 if args.use_cpu else 10  # Reduce workers for CPU
     testDataLoader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=num_workers)
 
@@ -100,10 +96,10 @@ def main(args):
     classifier = model.get_model(num_class, normal_channel=args.use_normals)
     if args.use_cpu:
         classifier = classifier.cpu()  # Load model to CPU
-        checkpoint = torch.load('/content/IntrA-3D-Objects-Classification/log/classification/pointnet2_ssg_wo_normals/checkpoints/best_model.pth', map_location=torch.device('cpu'))  # Load checkpoint on CPU
+        checkpoint = torch.load(ROOT_DIR+'/log/classification/pointnet2_ssg_wo_normals/checkpoints/best_model.pth', map_location=torch.device('cpu'))  # Load checkpoint on CPU
     else:
         classifier = classifier.cuda()
-        checkpoint = torch.load('/content/IntrA-3D-Objects-Classification/log/classification/pointnet2_ssg_wo_normals/checkpoints/best_model.pth')
+        checkpoint = torch.load(ROOT_DIR+'/log/classification/pointnet2_ssg_wo_normals/checkpoints/best_model.pth')
 
     classifier.load_state_dict(checkpoint['model_state_dict'])
      
