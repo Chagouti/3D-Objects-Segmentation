f3441d8a515afef75de6fe47b009f7a3bf736c0e
EL HOUCINE CHAGOUTI <138931526+Chagouti@users.noreply.github.com>
2024-10-12 17:29:19 +0100

    Update test_classification.py

--- test_classification.py
+++ test_classification.py
@@ -22,7 +22,7 @@ def parse_args():
     parser.add_argument('--use_cpu', action='store_true', default=True, help='use cpu mode')
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
     parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
-    
+    parser.add_argument('--model', default='pointnet_cls', help='model name [default: pointnet_cls]')
     # Set num_category to 2 for binary classification (IntrA has two classes)
     parser.add_argument('--num_category', default=2, type=int, choices=[2], help='training on IntrA dataset (binary classification)')
     
@@ -115,8 +115,7 @@ def main(args):
 
     '''MODEL LOADING'''
     num_class = args.num_category  # num_class is now 2 for binary classification
-    model_name = os.listdir(experiment_dir + '/logs')[0].split('.')[0]
-    print(os.listdir(experiment_dir + '/logs')[0])
+    model_name = args.model
     model = importlib.import_module(model_name)
 
     classifier = model.get_model(num_class, normal_channel=args.use_normals)
